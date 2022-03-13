6fea3b5d34dc89773fd70b777a22bd9a770de3da
alexeycherb <101508982+alexeycherb@users.noreply.github.com>
2022-03-13 13:27:46 +0000

    Create mini.js

---
+++ mini.js
@@ -0,0 +1,17 @@
+		d = new Date();
+		d.setHours(d.getHours(), d.getMinutes() + d.getTimezoneOffset());
+		Data = d.getDate();
+		Day = d.getDay();
+
+		DataStop = new Date (2022, 02, 12);
+        DataStop.setHours(DataStop.getHours() + 3, DataStop.getMinutes() + DataStop.getTimezoneOffset());
+        
+        if (d.getTime() < DataStop.getTime()) {
+             document.write(" ");   
+
+
+    }
+        else {
+            	document.getElementById('allrecords').style.display = 'none';
+
+        }
