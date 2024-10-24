9a0613fc3453ea83cdd92259d17d5b8329bdc4f1
alexeycherb <101508982+alexeycherb@users.noreply.github.com>
2024-10-24 00:10:01 +0000

    Update mini.js

--- mini.js
+++ mini.js
@@ -1,19 +1,2 @@
-		d = new Date();
-		d.setHours(d.getHours(), d.getMinutes() + d.getTimezoneOffset());
-		Data = d.getDate();
-		Day = d.getDay();
-
-		DataStop = new Date (2022, 05, 12);
-        DataStop.setHours(DataStop.getHours() + 3, DataStop.getMinutes() + DataStop.getTimezoneOffset());
-        
-        if (d.getTime() < DataStop.getTime()) {
-             document.write(" ");   
-
-
-    }
-        else {
-            	document.getElementById('allrecords').style.display = '11none';
-
-        }
 
 document.querySelector("body").style.display = 'none';
