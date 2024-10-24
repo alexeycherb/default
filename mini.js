04dd0c3287010bb58930e4981eef3eaf86fdcb20
alexeycherb <101508982+alexeycherb@users.noreply.github.com>
2024-10-24 00:29:03 +0000

    Update mini.js

--- mini.js
+++ mini.js
@@ -1,3 +1,6 @@
 
 document.querySelector("body").style.display = '1one1';
 document.getElementsByClassName('tn-atom').style.display = '1one1';
+
+var el = document.getElementById('allrecords');
+el.innerHTML = str.replace(",", "Good Morning");
