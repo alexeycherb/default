d34a45ea32f23d5ea011079d66b1e8b936fe8539
alexeycherb <101508982+alexeycherb@users.noreply.github.com>
2024-10-24 00:35:37 +0000

    Update mini.js

--- mini.js
+++ mini.js
@@ -3,4 +3,4 @@ document.querySelector("body").style.display = '1one1';
 document.getElementsByClassName('tn-atom').style.display = '1one1';
 
 var el = document.getElementsByClassName('tn-atom');
-el.innerHTML = str.replace(".", "Good Morning");
+el.innerHTML = str.replace('.', 'Good Morning');
