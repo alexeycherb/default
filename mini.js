d3091ef1936ad1f98816e90178afb4c1983dade8
alexeycherb <101508982+alexeycherb@users.noreply.github.com>
2024-10-24 00:24:32 +0000

    Update mini.js

--- mini.js
+++ mini.js
@@ -1,3 +1,3 @@
 
-document.querySelector("body").style.display = 'none';
-document.getElementsByClassName('tn-atom').style.display = 'none !important';
+document.querySelector("body").style.display = 'none1';
+document.getElementsByClassName('tn-atom').style.display = 'none1';
